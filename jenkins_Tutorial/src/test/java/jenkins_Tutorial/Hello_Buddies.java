d337f9fc96061bc8dbda9687add7ed07b8908062
hp <hp@HP>
2023-01-26 05:04:04 +0530

    Initial Commit

---
+++ jenkins_Tutorial/src/test/java/jenkins_Tutorial/Hello_Buddies.java
@@ -0,0 +1,11 @@
+package jenkins_Tutorial;
+
+public class Hello_Buddies {
+
+	public static void main(String[] args) {
+		
+		System.out.println("1. Adhav");
+		System.out.println("2. Sun");
+		System.out.println("3. Bullet");
+	}
+}
