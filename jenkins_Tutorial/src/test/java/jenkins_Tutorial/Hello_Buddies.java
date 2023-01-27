9da3aac64f76291aa65f98f085dd606affc3bc56
Adhavan0101 <122563548+Adhavan0101@users.noreply.github.com>
2023-01-27 08:59:28 +0530

    Add Kutty name
    I have add one more pet name in my house

--- jenkins_Tutorial/src/test/java/jenkins_Tutorial/Hello_Buddies.java
+++ jenkins_Tutorial/src/test/java/jenkins_Tutorial/Hello_Buddies.java
@@ -8,6 +8,7 @@ public class Hello_Buddies {
 		System.out.println("2. Sun");
 		System.out.println("3. Bullet");
 		System.out.println("4. Bhairava");
-		System.out.println("5. Puppy");
+		System.out.println("5. Kutty");
+		System.out.println("6. Puppy");
 	}
 }
